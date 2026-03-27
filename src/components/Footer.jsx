b631cf938608390403d4622a936b55978540241c
elfatrushiti-hash <elfat.rushiti@gmail.com>
2026-03-27 13:12:22 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,18 +2,19 @@ import React from "react";
 
 export default function Footer({ t }) {
   return (
-    <footer className="mt-12 py-6 text-center bg-gray-50 rounded-lg shadow-inner">
-      <p className="text-gray-700 font-medium">
-        {t.footer.text}{" "}
-        <a
-          href="https://www.intrum.ch/de/business-solutions/dienstleistungen/digital-onboarding/signing/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-intrumPurple font-semibold hover:underline"
-        >
-          {t.footer.link}
-        </a>
+    <footer className="mt-20 py-10 text-center text-gray-700 animate-fadeIn">
+      <p className="opacity-80 mb-2 text-sm">
+        {t.footer.text}
       </p>
+
+      <a
+        href="https://www.intrum.ch/de/business-solutions/dienstleistungen/digital-onboarding/signing/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-intrumPurple font-semibold hover:underline"
+      >
+        {t.footer.link}
+      </a>
     </footer>
   );
 }
